262f9ae793144e7cde09804853514bd7bb968396
Alya Kovalenko <kovalenkoalinam@gmail.com>
2024-03-06 19:13:36 +0300

    refactor: add artist folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { DbModule } from './db/db.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, DbModule],
+  imports: [ConfigModule.forRoot(), UserModule, DbModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService],
 })
