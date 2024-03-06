3fc088bfb08b2b139354cc69e1dc19e1477e82fa
Alya Kovalenko <kovalenkoalinam@gmail.com>
2024-03-06 21:23:28 +0300

    fix: updating in users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,17 +7,22 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 import { DbService } from 'src/db/db.service';
 import { v4 as uuidv4 } from 'uuid';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  constructor(private db: DbService) {}
+  users: User[] = [];
+
+  constructor(private db: DbService) {
+    this.db.users = this.users;
+  }
 
   findAll() {
-    return this.db.users;
+    return this.users;
   }
 
   findOne(id: string) {
-    return this.db.users.find((user) => user.id === id);
+    return this.users.find((user) => user.id === id);
   }
 
   create(createUserDto: CreateUserDto) {
@@ -33,7 +38,7 @@ export class UserService {
       updatedAt: currDate,
     };
 
-    this.db.users.push(newUser);
+    this.users.push(newUser);
 
     return newUser;
   }
@@ -41,11 +46,16 @@ export class UserService {
   updatePassword(id: string, updatePasswordDto: UpdatePasswordDto) {
     const { oldPassword, newPassword } = updatePasswordDto;
 
-    const foundIndex = this.db.users.findIndex((user) => user.id === id);
+    const foundIndex = this.users.findIndex((user) => user.id === id);
     if (foundIndex !== -1) {
-      if (this.db.users[foundIndex].password === oldPassword) {
-        this.db.users[foundIndex].password = newPassword;
-        return this.db.users[foundIndex];
+      if (this.users[foundIndex].password === oldPassword) {
+        this.users[foundIndex] = {
+          ...this.users[foundIndex],
+          password: newPassword,
+          updatedAt: Date.now(),
+          version: this.users[foundIndex].version + 1,
+        };
+        return this.users[foundIndex];
       } else {
         throw new ForbiddenException('OldPassword is wrong');
       }
@@ -55,9 +65,9 @@ export class UserService {
   }
 
   remove(id: string) {
-    const foundIndex = this.db.users.findIndex((user) => user.id === id);
+    const foundIndex = this.users.findIndex((user) => user.id === id);
     if (foundIndex !== -1) {
-      this.db.users.splice(foundIndex, 1);
+      this.users.splice(foundIndex, 1);
       return;
     } else {
       throw new NotFoundException();
