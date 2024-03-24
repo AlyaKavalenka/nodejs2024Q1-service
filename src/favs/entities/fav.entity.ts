40409360e86be07d28507e3982c9287f84dc1758
Alya Kavalenka <kovalenkoalinam@gmail.com>
2024-03-24 22:34:10 +0300

    feat: favs to typeorm

--- src/favs/entities/fav.entity.ts
+++ src/favs/entities/fav.entity.ts
@@ -1,5 +1,34 @@
-export class Fav {
-  artists: string[]; // favorite artists ids
-  albums: string[]; // favorite albums ids
-  tracks: string[]; // favorite tracks ids
+import { Album } from 'src/album/entities/album.entity';
+import { Artist } from 'src/artist/entities/artist.entity';
+import { Track } from 'src/track/entities/track.entity';
+import { Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class FavArtist {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @OneToOne(() => Artist)
+  @JoinColumn()
+  artist: Artist;
+}
+
+@Entity()
+export class FavAlbum {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @OneToOne(() => Album)
+  @JoinColumn()
+  album: Album;
+}
+
+@Entity()
+export class FavTrack {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @OneToOne(() => Track)
+  @JoinColumn()
+  track: Track;
 }
--- src/favs/favs.module.ts
+++ src/favs/favs.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FavsService } from './favs.service';
 import { FavsController } from './favs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Track } from 'src/track/entities/track.entity';
+import { Album } from 'src/album/entities/album.entity';
+import { Artist } from 'src/artist/entities/artist.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Track, Album, Artist])],
   controllers: [FavsController],
   providers: [FavsService],
 })
