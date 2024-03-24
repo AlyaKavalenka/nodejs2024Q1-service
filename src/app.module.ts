b9bf1b2a52a872b5f093c7ec9d411373e92ef8e8
Alya Kavalenka <kovalenkoalinam@gmail.com>
2024-03-24 22:35:54 +0300

    fix: error

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
-import { DbModule } from './db/db.module';
 import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
@@ -14,7 +13,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     ConfigModule.forRoot(),
     UserModule,
-    DbModule,
     ArtistModule,
     TrackModule,
     AlbumModule,
